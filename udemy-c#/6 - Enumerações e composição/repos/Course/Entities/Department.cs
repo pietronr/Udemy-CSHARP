14361d8f6495aca97d5ff45334bb2daac1a1d674
Pietro <54339762+im-pokz@users.noreply.github.com>
2021-01-20 12:07:50 -0300

    Add files via upload

---
+++ udemy-c#/6 - Enumerações e composição/repos/Course/Entities/Department.cs
@@ -0,0 +1,16 @@
+﻿namespace Course.Entities
+{
+    class Department
+    {
+        public string Name { get; set; }
+
+        public Department()
+        {
+        }
+
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+}
