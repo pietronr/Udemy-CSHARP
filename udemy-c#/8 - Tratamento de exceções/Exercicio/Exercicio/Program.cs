a8e58a6ec98450bd97fb856b4d8bc63ad3d82e32
Pietro <54339762+im-pokz@users.noreply.github.com>
2021-06-25 13:41:12 -0300

    Add files via upload

---
+++ udemy-c#/8 - Tratamento de exceções/Exercicio/Exercicio/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Exercicio.Entities;
+using Exercicio.Entities.Exceptions;
+using System.Globalization;
+
+namespace Exercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter account data:");
+                Console.Write("Number: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.Write("Holder: ");
+                string holder = Console.ReadLine();
+                Console.Write("Initial balance: ");
+                double balance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Withdraw limit: ");
+                double withdrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Account acc = new Account(number, holder, balance, withdrawLimit);
+
+                Console.Write("\nEnter amount for withdraw: ");
+                double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                acc.Withdraw(amount);
+
+                Console.WriteLine("New balance: " + acc.Balance.ToString("F2", CultureInfo.InvariantCulture));
+            }
+            catch(DomainException e)
+            {
+                Console.WriteLine("Withdraw error: " + e.Message);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Format error: " + e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unexpected error: " + e.Message);
+            }
+
+        }
+    }
+}
