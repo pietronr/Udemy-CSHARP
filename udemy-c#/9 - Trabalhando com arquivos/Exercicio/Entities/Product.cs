4f1a53292b582257e2ecf4b89f56c34ddf2b896e
Pietro <54339762+im-pokz@users.noreply.github.com>
2021-06-25 16:41:46 -0300

    Add files via upload

---
+++ udemy-c#/9 - Trabalhando com arquivos/Exercicio/Entities/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicio.Entities
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+
+        public Product()
+        {
+        }
+
+        public Product(string name, double price, int quantity)
+        {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public double Price()
+        {
+            return Price * Quantity;
+        }
+    }
+}
