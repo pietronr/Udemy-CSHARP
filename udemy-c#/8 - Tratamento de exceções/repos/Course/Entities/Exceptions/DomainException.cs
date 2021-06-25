7c06c9293ed1b2c2bacb7e289996d858a65a8a32
Pietro <54339762+im-pokz@users.noreply.github.com>
2021-06-25 12:37:29 -0300

    Add files via upload

---
+++ udemy-c#/8 - Tratamento de exceções/repos/Course/Entities/Exceptions/DomainException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Course.Entities.Exceptions
+{
+    class DomainException : ApplicationException
+    {
+        public DomainException(string message) : base(message)
+        {
+        }
+    }
+}
